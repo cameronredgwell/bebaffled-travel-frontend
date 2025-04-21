094c463adc33bae10e57a96f39cd86cab4fd3153
Cameron Redgwell <cam@Camerons-MacBook-Air.local>
2025-04-21 22:23:32 +0700

    Fix: Clean postcss config and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-=======
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
->>>>>>> a179509 (Working frontend structure)
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> a179509 (Working frontend structure)
